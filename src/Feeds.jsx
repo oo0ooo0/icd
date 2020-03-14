3917683af48799feb6eec9af0cb56929c1f3e6bc
oo0ooo0 <nyanyanyangnyang@gmail.com>
2020-03-14 14:19:01 +0900

    react-masonry-css 라이브러리 적용

--- src/Feeds.jsx
+++ src/Feeds.jsx
@@ -1,17 +1,103 @@
 import React from 'react';
 import Item from './components/Item';
-import './Feeds.css';
+import './Feeds.scss';
+import Masonry from 'react-masonry-css';
 
 function Feeds() {
   return (
     <main id='Feeds' className='items'>
       <div className='container'>
-        <h2>Feed</h2>
+        <h2 className='title'>Feed</h2>
         <h3>최신 패션 뉴스를 전해드립니다.</h3>
-        <div className='item'>item</div>
-        <div className='item'>item</div>
-        <div className='item'>item</div>
-        {/* <Item /> */}
+        <Masonry
+          breakpointCols={3}
+          className='my-masonry-grid'
+          columnClassName='my-masonry-grid_column'
+        >
+          <div className='item'>
+            11111111111 item1 item1 item1 item1 item1 item1 item1 item1 item1item1 item1 item1 item1
+            item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1item1
+            item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1
+            item1 item1 item1item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1
+            item1 item1 item1 item1 item1 item1item1 item1 item1 item1 item1 item1 item1 item1 item1
+            item1 item1 item1 item1 item1 item1 item1 item1 item1item1 item1 item1 item1 item1 item1
+            item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1item1 item1 item1
+            item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1
+            item1item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1
+            item1 item1 item1 item1item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1
+            item1 item1 item1 item1 item1 item1 item1item1 item1 item1 item1 item1 item1 item1 item1
+            item1
+          </div>
+          <div className='item'>
+            2222222222 item1 item1 item1 item1 item1 item1 item1 item1 item1item1 item1 item1 item1
+            item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1item1
+            item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1
+            item1 item1 item1item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1
+            item1 item1 item1 item1 item1 item1item1 item1 item1 item1 item1 item1 item1 item1 item1
+            item1 item1 item1 item1 item1 item1 item1 item1 item1item1 item1 item1 item1 item1 item1
+            item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1item1 item1 item1
+            item1 item1
+          </div>
+          <div className='item'>
+            33333333 item1 item1 item1 item1 item1 item1 item1 item1 item1item1 item1 item1 item1
+            item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1item1
+            item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1
+            item1 item1 item1item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1
+            item1 item1 item1 item1 item1 item1item1 item1 item1 item1 item1 item1 item1 item1 item1
+            item1 item1 item1 item1 item1 item1 item1 item1 item1item1 item1 item1 item1 item1 item1
+            item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1item1 item1 item1
+            item1 item1
+          </div>
+          <div className='item'>
+            44 item1 item1 item1 item1 item1 item1 item1 item1 item1item1 item1 item1 item1 item1
+            item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1item1 item1
+            item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1
+            item1 item1item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1
+            item1 item1 item1 item1 item1item1 item1 item1 item1 item1 item1 item1 item1 item1 item1
+            item1 item1 item1 item1 item1 item1 item1 item1item1 item1 item1 item1 item1 item1 item1
+            item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1item1 item1 item1 item1
+            item1
+          </div>
+          <div className='item'>
+            55 item1 item1 item1 item1 item1 item1 item1 item1 item1item1 item1 item1 item1 item1
+            item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1item1 item1
+            item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1
+            item1 item1item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1
+            item1 item1 item1 item1 item1item1 item1 item1 item1 item1 item1 item1 item1 item1 item1
+            item1 item1 item1 item1 item1 item1 item1 item1item1 item1 item1 item1 item1 item1 item1
+            item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1item1 item1 item1 item1
+            item1
+          </div>
+          <div className='item'>
+            66 item1 item1 item1 item1 item1 item1 item1 item1 item1item1 item1 item1 item1 item1
+            item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1item1 item1
+            item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1
+            item1 item1item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1
+            item1 item1 item1 item1 item1item1 item1 item1 item1 item1 item1 item1 item1 item1 item1
+            item1 item1 item1 item1 item1 item1 item1 item1item1 item1 item1 item1 item1 item1 item1
+            item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1item1 item1 item1 item1
+            item1
+          </div>
+          <div className='item'>77</div>
+          <div className='item'>
+            88 item1 item1 item1 item1 item1 item1 item1 item1 item1item1 item1 item1 item1 item1
+            item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1item1 item1
+            item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1
+            item1 item1item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1
+            item1 item1 item1 item1 item1item1 item1 item1 item1 item1 item1 item1 item1 item1 item1
+            item1 item1 item1 item1 item1 item1 item1 item1item1 item1 item1 item1 item1 item1 item1
+            item1 item1 item1 item1 item1 item1 item1 item1 item1 item1 item1item1 item1 item1 item1
+            item1
+          </div>
+          <div className='item'>item4</div>
+          <div className='item'>item5</div>
+          <div className='item'>item1</div>
+          <div className='item'>item2</div>
+          <div className='item'>item3</div>
+          <div className='item'>item4</div>
+          <div className='item'>item5</div>
+          {/* <Item /> */}
+        </Masonry>
       </div>
     </main>
   );
