4f9638658d0062eb04340483b6dac000a3be78d4
oo0ooo0 <nyanyanyangnyang@gmail.com>
2020-03-20 12:04:25 +0900

    fixed createdAt shorter
    **need to Refactoring

--- src/services/store.jsx
+++ src/services/store.jsx
@@ -8,6 +8,7 @@ function arrayToKeyValue(array) {
   for (let i = 0; i < array.length; i++) {
     result[array[i].id] = array[i];
     result[array[i].id].isLiked = false;
+    result[array[i].id].createdAt = array[i].createdAt.slice(0, 10);
   }
   return result;
 }
@@ -15,6 +16,26 @@ function arrayToKeyValue(array) {
 function arrayToKeyValue2(array) {
   let result = {};
   for (let i = 0; i < array.length; i++) {
+    /* arrayToKeyValue랑 다르게 array안에 id랑 createdAt이 같이 있으니까  
+    result[array[i].id] = array[i];같은 구조변화가 없으니까
+    먼저 넣어주고 나서 result값이 정해져도 날짜를 바꿀 수 있음 
+    */
+    console.log(array[i]);
+    array[i].createdAt = array[i].createdAt.slice(0, 10);
+
+    // {
+    //   array[i].replies && console.log('꺄', array[i].replies.list);
+    // }
+    if (array[i].replies) {
+      for (let j = 0; j < array[i].replies.list.length; j++) {
+        array[i].replies.list[j].createdAt = array[i].replies.list[j].createdAt.slice(0, 10);
+        // console.log('꺄', array[i].replies.list[j].createdAt);
+      }
+    }
+
+    if (array[i].replies) {
+      // array[i].replies[i].createdAt = array[i].replies.createdAt.slice(0, 10);
+    }
     if (result[array[i].feedId]) {
       if (result[array[i].feedId].comments) {
         result[array[i].feedId].comments.push(array[i]);
@@ -33,17 +54,13 @@ function arrayToKeyValue2(array) {
   return result;
 }
 
-// console.log('list', dummyCommentsData.data.list);
-
 const initialState = {
   feeds: arrayToKeyValue(dummyFeedsData.data.list),
   comments: arrayToKeyValue2(dummyCommentsData.data.list),
 };
-console.log('store', initialState.comments);
 
 const store = createStore(
   rootReducer,
-  // key value 로 변환
   initialState,
   process.env.NODE_ENV === 'development'
     ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
