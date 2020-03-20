46c41634144b508574d041a09f4ca19d676a471c
oo0ooo0 <nyanyanyangnyang@gmail.com>
2020-03-20 14:53:37 +0900

    remove min-width

--- src/components/Containers.jsx
+++ src/components/Containers.jsx
@@ -4,7 +4,6 @@ export const LayoutContainer = styled.div`
   width: ${props => props.width || 92}%;
   margin: 10px auto;
   position: relative;
-  min-width: 960px;
 `;
 
 export const MainContainer = styled(LayoutContainer)`
