32270b65459f09d1fc3c157a274d584f6821a4f2
oo0ooo0 <nyanyanyangnyang@gmail.com>
2020-03-20 14:52:55 +0900

    fixed for responsive header

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,22 +7,26 @@ const StyledHeader = styled.header`
   top: 0;
   background-color: #fff;
   z-index: 10;
+
   .header-cont {
     width: 92%;
     margin: auto;
+    display: flex;
   }
+
   .top-menu-wrap {
     margin-top: 10px;
     .top-menu {
       display: flex;
       justify-content: flex-end;
+        width:100%;
+        li {
+          margin-left: 50px;
 
-      li {
-        margin-left: 50px;
-
-        a {
-          font: normal 400 12px/1 'Lato';
-          color: #707070;
+          a {
+            font: normal 400 12px/1 'Lato';
+            color: #707070;
+          }
         }
       }
     }
@@ -32,39 +36,45 @@ const StyledHeader = styled.header`
     margin-top: 10px;
     border-top: 1px solid lightGray;
     border-bottom: 1px solid lightGray;
-    min-width: 960px;
-    .header-cont {
+      .logo {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .gnb {
+      width: 100%;
       display: flex;
 
-      .logo {
+      ul {
+        margin-left: 200px;
+        width: 100%;
         display: flex;
+        justify-content: space-between;
         align-items: center;
-        h1 {
+
+        li {
+          padding: 30px 0px;
           a {
+            margin-top: 10px;
+            display: inline-block;
+            font: normal 900 17px/1 'Lato';
+            color: #1d1e21;
           }
         }
       }
+    }
+  }
 
-      .gnb {
+  @media (max-width: 960px) {
+    .main-menu-wrap {
+      .header-cont {
+        display: block;
         width: 100%;
-        display: flex;
-
-        ul {
-          margin-left: 200px;
-          width: 100%;
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-
-          li {
-            padding: 30px 0px;
-            a {
-              margin-top: 10px;
-              display: inline-block;
-              font: normal 900 17px/1 'Lato';
-              color: #1d1e21;
-            }
-          }
+        .gnb ul{
+          justify-content:space-between;
+          margin-left:0px;
+          padding: 0px 10px;
         }
       }
     }
